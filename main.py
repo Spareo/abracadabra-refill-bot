1bf6f2ce1ccbb83ba4346eb4d214438a1f8d1c55
Dmitry Khenkin <dkhenkin@crossriverbank.com>
2021-11-20 16:46:02 -0600

    Updated to just care about wMEMO MIMs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 from decimal import Decimal
 from web3 import Web3
 import discordWH
-import twitter
+# import twitter
 
 
 cauldrons = json.loads(open("Cauldrons.json", 'r').read()) #Loads Cauldrons.json as a nested dic
@@ -18,6 +18,12 @@ def checkTreshold(previous_amount, amount, treshold):
         else:
             return False
 
+def isGreaterThanZero(amount):
+    if amount > 0:
+        return True
+    else:
+        return False
+
 for chain in settings.keys(): #Go though each chain
     w3 = Web3(Web3.HTTPProvider(settings[chain]['RPC'])) #Network RPC
     bb_address = w3.toChecksumAddress(settings[chain]['bentobox']) #BentoBox Contract Address
@@ -28,23 +34,27 @@ for chain in settings.keys(): #Go though each chain
     def  getMIMAmount(mim_address, cauldron):
         mim_amount=bentobox.functions.balanceOf(mim_address, cauldron).call()
         mim_amount=w3.fromWei(mim_amount, 'ether')
-        return mim_amount
+        return Decimal(mim_amount)
 
 
     for tokens in cauldrons.keys(): #Go through each Cauldron entry
         if cauldrons[tokens]['chain'] == chain: #Check wether the Cauldron entry is on the chain we are working with 
-            amount=getMIMAmount(MIM_contract_address, w3.toChecksumAddress(cauldrons[tokens]['address'])) #Gets MIM available for the cauldron
-            
-            if checkTreshold(Decimal(cauldrons[tokens]['previous_amount']), Decimal(amount), Decimal(settings[chain]['threshold'])): #Compare amount with previous amount and check if above threshold, defined per chain
-                print("%s on %s:" %(tokens, chain))
-                print("Old amount : ", cauldrons[tokens]['previous_amount'])
-                print("New amount : ", amount)
-                print("-----")
+            amount = getMIMAmount(MIM_contract_address, w3.toChecksumAddress(cauldrons[tokens]['address'])) #Gets MIM available for the cauldron
+            threshold = Decimal(settings[chain]['threshold'])
+             
+            print("%s on %s:" %(tokens, chain))
+            # print("Old amount : ", cauldrons[tokens]['previous_amount'])
+            # print("New amount : ", amount)
+            print("Current MIMs available: ", amount)
+            print("-----")
+
+            if checkTreshold(0, amount, threshold): 
                 try:
                     discordWH.sendMessage(tokens, amount, cauldrons, settings, chain) #Send discord msg
                 except:
                     print("error sending discord message")
-                twitter.tweet(tokens, amount, settings, chain)
+                #twitter.tweet(tokens, amount, settings, chain)
+
                 
 
             cauldrons[tokens]['previous_amount']=str(amount) #Store amount as Previous_amount
