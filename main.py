b5687ae7711889c23e03a3a840eb95ab3f39c527
Dmitry Khenkin <dkhenkin@crossriverbank.com>
2021-11-20 17:12:07 -0600

    Switched to using threshold with some percentage to reduce spam to discord

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import decimal
 import json
+import time
 from decimal import Decimal
 from web3 import Web3
 import discordWH
@@ -18,46 +19,42 @@ def checkTreshold(previous_amount, amount, treshold):
         else:
             return False
 
-def isGreaterThanZero(amount):
-    if amount > 0:
-        return True
-    else:
-        return False
+def  getMIMAmount(mim_address, cauldron):
+    mim_amount=bentobox.functions.balanceOf(mim_address, cauldron).call()
+    mim_amount=w3.fromWei(mim_amount, 'ether')
+    return Decimal(mim_amount)
 
-for chain in settings.keys(): #Go though each chain
-    w3 = Web3(Web3.HTTPProvider(settings[chain]['RPC'])) #Network RPC
-    bb_address = w3.toChecksumAddress(settings[chain]['bentobox']) #BentoBox Contract Address
-    MIM_contract_address=w3.toChecksumAddress(settings[chain]['MIM']) #MIM Contract Address
-    bb_ABI = json.load(open('BentoBoxV1.json', 'r')) #BentoBox ABI load, from BentoBoxV1.json
-    bentobox = w3.eth.contract(address=bb_address, abi=bb_ABI) 
+while True:
+    for chain in settings.keys(): #Go though each chain
+        w3 = Web3(Web3.HTTPProvider(settings[chain]['RPC'])) #Network RPC
+        bb_address = w3.toChecksumAddress(settings[chain]['bentobox']) #BentoBox Contract Address
+        MIM_contract_address=w3.toChecksumAddress(settings[chain]['MIM']) #MIM Contract Address
+        bb_ABI = json.load(open('BentoBoxV1.json', 'r')) #BentoBox ABI load, from BentoBoxV1.json
+        bentobox = w3.eth.contract(address=bb_address, abi=bb_ABI) 
 
-    def  getMIMAmount(mim_address, cauldron):
-        mim_amount=bentobox.functions.balanceOf(mim_address, cauldron).call()
-        mim_amount=w3.fromWei(mim_amount, 'ether')
-        return Decimal(mim_amount)
+        for tokens in cauldrons.keys(): #Go through each Cauldron entry
+            if cauldrons[tokens]['chain'] == chain: #Check wether the Cauldron entry is on the chain we are working with 
 
-
-    for tokens in cauldrons.keys(): #Go through each Cauldron entry
-        if cauldrons[tokens]['chain'] == chain: #Check wether the Cauldron entry is on the chain we are working with 
-            amount = getMIMAmount(MIM_contract_address, w3.toChecksumAddress(cauldrons[tokens]['address'])) #Gets MIM available for the cauldron
-            threshold = Decimal(settings[chain]['threshold'])
-             
-            print("%s on %s:" %(tokens, chain))
-            # print("Old amount : ", cauldrons[tokens]['previous_amount'])
-            # print("New amount : ", amount)
-            print("Current MIMs available: ", amount)
-            print("-----")
-
-            if checkTreshold(0, amount, threshold): 
-                try:
-                    discordWH.sendMessage(tokens, amount, cauldrons, settings, chain) #Send discord msg
-                except:
-                    print("error sending discord message")
-                #twitter.tweet(tokens, amount, settings, chain)
-
+                previous_amount = Decimal(cauldrons[tokens]['previous_amount'])
+                amount = getMIMAmount(MIM_contract_address, w3.toChecksumAddress(cauldrons[tokens]['address'])) #Gets MIM available for the cauldron
+                threshold = Decimal(settings[chain]['threshold'])
                 
+                print("%s on %s:" %(tokens, chain))
+                # print("Old amount : ", cauldrons[tokens]['previous_amount'])
+                # print("New amount : ", amount)
+                print("Current MIMs available: ", amount)
+                print("-----")
 
-            cauldrons[tokens]['previous_amount']=str(amount) #Store amount as Previous_amount
+                if checkTreshold(previous_amount, amount, threshold): 
+                    try:
+                        discordWH.sendMessage(tokens, amount, cauldrons, settings, chain) #Send discord msg
+                    except:
+                        print("error sending discord message")
+                    #twitter.tweet(tokens, amount, settings, chain)
+
+                    
+
+                cauldrons[tokens]['previous_amount'] = str(amount) #Store amount as Previous_amount
             
     
-json.dump(cauldrons, open("Cauldrons.json", 'w'), indent=4, sort_keys=True)
+    json.dump(cauldrons, open("Cauldrons.json", 'w'), indent=4, sort_keys=True)
